d6736b0fe182b40ff465be2f0308dbe05b9f6b45
Anıl <anil@Anl-MacBook-Air.local>
2024-01-16 22:22:36 +0300

    UI completed

--- App.js
+++ App.js
@@ -1,20 +1,87 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from 'react-native';
+import { colors } from './src/constants';
+import Keyboard from './src/components/Keyboard';
+
+const NUMBER_OF_TRIES = 6;
+
+const copyArray = (arr) => {
+  return [...arr.map((rows) => [...rows])];
+};
 
 export default function App() {
+  const word = 'hello';
+  const letters = word.split('');
+
+  const [rows, setRows] = useState(
+    new Array(NUMBER_OF_TRIES).fill(new Array(letters.length).fill(''))
+  );
+
+  const [curRow, setCurRow] = useState(0);
+  const [curCol, setCurCol] = useState(0);
+
+  const onKeyPressed = (key) => {
+    const updatedRows = copyArray(rows);
+    rows[curRow][curCol] = key;
+    setRows(updatedRows);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style='auto' />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style='light' />
+      <Text style={styles.title}>WORDLE</Text>
+      <ScrollView style={styles.map}>
+        {rows.map((row) => (
+          <View style={styles.row}>
+            {row.map((cell) => (
+              <View style={styles.cell}>
+                <Text style={styles.cellText}>{cell.toUpperCase()}</Text>
+              </View>
+            ))}
+          </View>
+        ))}
+      </ScrollView>
+      <Keyboard onKeyPressed={onKeyPressed} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.black,
     alignItems: 'center',
+  },
+  title: {
+    color: colors.lightgrey,
+    fontSize: 32,
+    fontWeight: 'bold',
+    letterSpacing: 7,
+  },
+  map: {
+    alignSelf: 'stretch',
+    height: 100,
+    marginVertical: 20,
+  },
+  row: {
+    alignSelf: 'stretch',
+    flexDirection: 'row',
     justifyContent: 'center',
+  },
+  cell: {
+    borderWidth: 3,
+    borderColor: colors.darkgrey,
+    flex: 1,
+    aspectRatio: 1,
+    margin: 3,
+    maxWidth: 70,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cellText: {
+    color: colors.lightgrey,
+    fontWeight: 'bold',
+    fontSize: 28,
   },
 });
