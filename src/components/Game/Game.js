cf692d5ec8424ba18456d4d106ff0987e81c6def
Anıl <anil@Anl-MacBook-Air.local>
2024-01-20 21:33:59 +0300

    storage implementation

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react';
-import { Text, View, ScrollView, Alert } from 'react-native';
+import { Text, View, ScrollView, Alert, ActivityIndicator } from 'react-native';
 import { colors, CLEAR, ENTER, colorsToEmoji } from '../../constants';
 import Keyboard from '../Keyboard';
 import * as Clipboard from 'expo-clipboard';
 import words from '../../words';
 import styles from './Game.styles';
 import { copyArray, getDayOfTheYear } from '../../utils';
+import AsyncSorage from '@react-native-async-storage/async-storage';
 
 const NUMBER_OF_TRIES = 6;
 
@@ -22,12 +23,49 @@ const Game = () => {
   const [curRow, setCurRow] = useState(0);
   const [curCol, setCurCol] = useState(0);
   const [gameState, setGameState] = useState('playing');
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     if (curRow > 0) {
       checkGameState();
     }
   }, [curRow]);
+
+  useEffect(() => {
+    if (loaded) {
+      presistState();
+    }
+  }, [rows, curRow, curCol, gameState]);
+
+  useEffect(() => {
+    readState();
+  }, []);
+
+  const presistState = async () => {
+    //  white all the state variables in async storage
+    const data = {
+      rows,
+      curRow,
+      curCol,
+      gameState,
+    };
+    const dataString = JSON.stringify(data);
+    await AsyncSorage.setItem('@game', dataString);
+  };
+
+  const readState = async () => {
+    const dataString = await AsyncSorage.getItem('@game');
+    try {
+      const data = JSON.parse(dataString);
+      setRows(data.rows);
+      setCurCol(data.curCol);
+      setCurRow(data.curRow);
+      setGameState(data.gameState);
+    } catch (e) {
+      console.log('Could not parse the state');
+    }
+    setLoaded(true);
+  };
 
   const checkGameState = () => {
     if (checkIfWon() && gameState !== 'won') {
@@ -122,6 +160,10 @@ const Game = () => {
   const greenCaps = getAllLettersWithColor(colors.primary);
   const yellowCaps = getAllLettersWithColor(colors.secondary);
   const greyCaps = getAllLettersWithColor(colors.darkgrey);
+
+  if (!loaded) {
+    return <ActivityIndicator />;
+  }
 
   return (
     <>
